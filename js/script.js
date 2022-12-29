7722f320751610e840476a47e300dfa67209c73f
Gautam <gautampandit797@gmail.com>
2022-12-30 00:35:15 +0530

    MY PERSONAL PORTFOLIO

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ------============================typing animation=======================*/
+var typed = new Typed(".typing",{
+    strings:["","web Designer", "Web Developer","Graphics Designer","Hero"],
+    typespeed:100,
+    Backspeed:60,
+    loop:true
+}) 
